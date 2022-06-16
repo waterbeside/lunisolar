8dc92d3c5263723ca35168b49ea49b6acbb51953
Natron Kan <waterbeside@163.com>
2022-06-16 22:49:34 +0800

    feat: 八字类

---
+++ src/class/char8.ts
@@ -0,0 +1,92 @@
+import { SB } from './stemBranch'
+import { Term } from './term'
+import * as U from '../utils'
+
+export class Char8 {
+  private _value: number = -1
+  private _list: SB[] = []
+  constructor(sbList: [SB, SB, SB, SB]) {
+    this._list = sbList
+    this._value = this._computeValue(sbList)
+  }
+
+  get value() {
+    return this._value
+  }
+
+  get list() {
+    return this._list
+  }
+
+  get year() {
+    return this._list[0]
+  }
+
+  get month() {
+    return this._list[1]
+  }
+
+  get day() {
+    return this._list[2]
+  }
+
+  get twoHour() {
+    return this._list[3]
+  }
+  // 日主
+  get me() {
+    return this._list[2].stem
+  }
+
+  private _computeValue(sbList: SB[] = this._list) {
+    let res = 0
+    for (let i = 0; i < 4; i++) {
+      res += sbList[i].valueOf() * Math.pow(10, 2 * (3 - i))
+    }
+    return res
+  }
+
+  static computeSBYear(date: Date | number, changeEgeTrem: number | null = null) {
+    let year = typeof date !== 'number' ? date.getFullYear() : date
+    if (changeEgeTrem !== null && typeof date !== 'number') {
+      changeEgeTrem = changeEgeTrem % 24
+      let isPreYear = changeEgeTrem >= 0
+      changeEgeTrem = changeEgeTrem >= 0 ? changeEgeTrem : 24 + changeEgeTrem
+      // 查出当前节气日期
+      let yearStart = date.getFullYear()
+      if (isPreYear) yearStart--
+      const yearEnd = yearStart + 1
+      // 该年的岁的范围
+      const startTermDate = Term.findDate(yearStart, changeEgeTrem)
+      const endTermDate = Term.findDate(yearEnd, changeEgeTrem)
+      const startDate = U.toDate(
+        `${startTermDate[0]}-${startTermDate[1]}-${startTermDate[2] - 1} 23:00:00`
+      )
+      const endDate = U.toDate(`${endTermDate[0]}-${endTermDate[1]}-${endTermDate[2] - 1} 23:00:00`)
+      // 检查是否在该岁的范围内
+      if (date.valueOf() < startDate.valueOf()) yearStart--
+      else if (date.valueOf() >= endDate.valueOf()) yearStart++
+      year = yearStart
+    }
+    const stemValue = (year - 4) % 10
+    const branchValue = (year - 4) % 12
+    return new SB(stemValue, branchValue)
+  }
+
+  static computeSBMonth(date: Date) {
+    // 知道该日是哪个节气之后便可知道该日是哪个地支月
+    // const nodeValue = Term.findNode(date, true)
+    // const BranchValue = nodeValue / 2
+    // 求月天干 （2018年12月大雪乃甲子月）
+    const monthDiff = Math.abs((date.getFullYear() - 2018) * 12 + date.getMonth() + 1)
+    return new SB(monthDiff)
+  }
+
+  toString() {
+    return `${this.year}${this.month}${this.day}${this.twoHour}`
+  }
+
+  valueOf() {
+    return this._value
+  }
+}
