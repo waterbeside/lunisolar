b3d33c7e4a44678a204e45b77855b4675c2bbe67
Natron Kan <waterbeside@163.com>
2022-07-13 15:27:38 +0800

    chore: 打包plugin目录的插件

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,5 @@
+import fs from 'fs'
+import path from 'path'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import ts from 'rollup-plugin-typescript2'
@@ -11,22 +13,59 @@ const extensions = ['.js', '.jsx', '.ts', '.tsx']
 const tsPlugin = ts({ extensions })
 const terserPlugin = terser({ format: { comments: false } })
 
-export default [
-  {
-    input: 'src/index.ts',
-    output: [
-      {
-        // UMD for browser-friendly build
-        name: 'lunisolar',
-        file: pkg.browser,
-        format: 'umd'
-      },
-      {
-        // ES module for bundlers build
-        file: pkg.module,
-        format: 'es'
-      }
-    ],
-    plugins: [clear({ targets: ['dist'] }), resolve(), commonjs(), tsPlugin, terserPlugin]
+// fileName： pkg.module,
+const configFactory = config => {
+  const { input, fileName, filePath, name, isClear, createEs } = config
+  // load rollup plugins
+  const plugins = [resolve(), commonjs(), tsPlugin, terserPlugin]
+  if (isClear) plugins.unshift(clear({ targets: ['dist'] }))
+  // output
+  const output = [
+    {
+      // UMD for browser-friendly build
+      name: name || 'lunisolar',
+      file: filePath && fileName ? path.join(filePath, `${fileName}.js`) : pkg.main,
+      format: 'umd'
+    }
+  ]
+  if (createEs) {
+    output.push({
+      // ES module for bundlers build
+      file: fileName ? path.join(filePath, `${fileName}.esm.js`) : pkg.module,
+      format: 'es'
+    })
   }
+
+  return {
+    input,
+    output,
+    plugins
+  }
+}
+
+const configs = [
+  configFactory({
+    input: 'src/index.ts',
+    isClear: true,
+    createEs: true
+  })
 ]
+
+const configPlugins = () => {
+  const pluginPath = path.join(__dirname, 'src', 'plugins')
+  const pluginNames = fs.readdirSync(pluginPath)
+  for (const pName of pluginNames) {
+    configs.push(
+      configFactory({
+        name: pName,
+        input: path.join(pluginPath, pName, 'index.ts'),
+        filePath: path.join(__dirname, 'dist', 'plugins'),
+        fileName: pName
+      })
+    )
+  }
+}
+
+configPlugins()
+
+export default configs
