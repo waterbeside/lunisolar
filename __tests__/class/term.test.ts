806cfcd45e741f3ab1c9ed2ac370974ada9f8fd6
Natron Kan <waterbeside@163.com>
2022-07-28 21:06:30 +0800

    test: 添加SolarTerm.findNode方法的相关测试用例

--- __tests__/class/term.test.ts
+++ __tests__/class/term.test.ts
@@ -13,17 +13,38 @@ describe('test the Term class', () => {
   it('2022-03-20 春分', () => {
     expect(SolarTerm.findDate(2022, '春分')).toEqual([2022, 3, 20])
   })
+
+  it('2021-12-21 春分', () => {
+    expect(SolarTerm.findDate(2021, '冬至')).toEqual([2021, 12, 21])
+  })
 })
 
 describe('test Term findNode', () => {
   it('test', () => {
     expect(SolarTerm.findNode(parseDate('2022-07-24'), { nodeFlag: 1, returnValue: true })).toEqual(
-      [new SolarTerm('大暑').valueOf(), 23]
+      [new SolarTerm('大暑').valueOf(), parseDate('2022-07-23')]
     )
   })
-  it('test', () => {
+  it('test2', () => {
     expect(SolarTerm.findNode(parseDate('2022-07-24'), { nodeFlag: 0, returnValue: true })).toEqual(
-      [new SolarTerm('小暑').valueOf(), 7]
+      [new SolarTerm('小暑').valueOf(), parseDate('2022-07-07')]
     )
+  })
+
+  it('test3', () => {
+    expect(SolarTerm.findNode(parseDate('2022-1-1'), { nodeFlag: 0, returnValue: true })).toEqual([
+      new SolarTerm('大雪').valueOf(),
+      parseDate('2021-12-07')
+    ])
+
+    expect(SolarTerm.findNode(parseDate('2022-1-1'), { nodeFlag: 1, returnValue: true })).toEqual([
+      new SolarTerm('冬至').valueOf(),
+      parseDate('2021-12-21')
+    ])
+
+    expect(SolarTerm.findNode(parseDate('2022-1-1'), { nodeFlag: 2, returnValue: true })).toEqual([
+      new SolarTerm('冬至').valueOf(),
+      parseDate('2021-12-21')
+    ])
   })
 })
