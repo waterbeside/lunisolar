a3dec92ff0e861f5c3680c1ba4694b051e48c093
Natron Kan <waterbeside@163.com>
2022-08-24 16:04:29 +0800

    五行纳音相关语言包

---
+++ src/plugins/takeSound/locale/en.ts
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+
+const locale = {
+  name: 'en',
+  takeSound: [
+    'Gold in the sea', 'Fire in the furnace', 'Big forest trees', 'Roadside soil',
+    'Swordsman Gold', 'Mountain Fire', 'Stream into the water', 'City gate soil',
+    'White gold', 'willow wood', 'Well spring water', 'The soil on the house',
+    'Human Torch', 'Pine and Cypress', 'Long Flowing Water', 'Gold in Sand',
+    'Fire under the mountain', 'Plain wood', 'Soil on the wall', 'Gold foil',
+    'Covered Lights', 'The water of the heavenly river', 'Big Post Earth', 'Hairpin gold',
+    'Mulberry wood', 'Big stream water', 'Sand in the soil', 'Fire in the sky',
+    'Pomegranate Wood', 'Sea Water'
+  ]
+}
+
+export default locale
