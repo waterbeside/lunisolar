ada9b49b2064563e3aa9829b7d65667b2586b9e6
Natron Kan <waterbeside@163.com>
2022-09-28 21:31:15 +0800

    test: 测试长生十二神

--- __tests__/plugins/theGods/index.test.ts
+++ __tests__/plugins/theGods/index.test.ts
@@ -1,19 +1,53 @@
 import lunisolar from '../../../src/index'
 import theGods from '../../../src/plugins/theGods'
+import { God } from '../../../src/plugins/theGods/class/god'
+import type { TheGods } from '../../../src/plugins/theGods/class/theGods'
 
 lunisolar.extend(theGods)
 
 interface Lunisolar extends ILunisolar {
-  duty12God: string
+  // duty12God: God
+  theGods: TheGods
 }
 
-describe('plugins/theGods', () => {
+describe('plugins/theGods duty12God', () => {
   it('2022-08-16', () => {
     const lsr = lunisolar('2022-08-16') as unknown as Lunisolar
-    expect(lsr.duty12God).toBe('執')
+    expect(lsr.theGods.getDuty12God().toString()).toBe('執')
   })
   it('2022-08-25', () => {
     const lsr = lunisolar('2022-08-25') as unknown as Lunisolar
-    expect(lsr.duty12God).toBe('滿')
+    expect(lsr.theGods.getDuty12God().name).toBe('滿')
+  })
+})
+
+describe('plugins/theGods life12God', () => {
+  it('2022-09-28', () => {
+    const lsr = lunisolar('2022-09-28') as unknown as Lunisolar
+    expect(lsr.theGods.getLife12God('day').name).toBe('絕')
+  })
+  it('2022-09-29', () => {
+    const lsr = lunisolar('2022-09-29') as unknown as Lunisolar
+    expect(lsr.theGods.getLife12God('day').name).toBe('絕')
+  })
+  it('2022-09-30', () => {
+    const lsr = lunisolar('2022-09-30') as unknown as Lunisolar
+    expect(lsr.theGods.getLife12God('day').name).toBe('墓')
+  })
+  it('2022-10-01', () => {
+    const lsr = lunisolar('2022-10-01') as unknown as Lunisolar
+    expect(lsr.theGods.getLife12God('day').name).toBe('胎')
+  })
+  it('2022-10-2', () => {
+    const lsr = lunisolar('2022-10-2') as unknown as Lunisolar
+    expect(lsr.theGods.getLife12God('day').name).toBe('胎')
+  })
+  it('2022-10-3', () => {
+    const lsr = lunisolar('2022-10-3') as unknown as Lunisolar
+    expect(lsr.theGods.getLife12God('day').name).toBe('墓')
+  })
+  it('2022-10-4', () => {
+    const lsr = lunisolar('2022-10-4') as unknown as Lunisolar
+    expect(lsr.theGods.getLife12God('day').name).toBe('絕')
   })
 })
