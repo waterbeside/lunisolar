248bb36976805e40f6907736e9c95e99eb8dbeba
Natron Kan <waterbeside@163.com>
2022-09-29 15:42:12 +0800

    dev 等弟查詢

--- src/plugins/theGods/goodBadLevel.ts
+++ src/plugins/theGods/goodBadLevel.ts
@@ -1,3 +1,5 @@
+import type { TheGods } from './class/theGods'
+import type { God } from './class/god'
 /*
 -----
 宜忌等第表
@@ -14,7 +16,63 @@
 
 const theDeList = ['歲德', '歲德合', '月德', '月德合', '天德', '天德合']
 
-const getGoodBadLevel = () => ({
+const levelKeyList = [
+  '平日',
+  '收日',
+  '閉日',
+  '劫煞',
+  '災煞',
+  '月煞',
+  '月刑',
+  '月害',
+  '月厭',
+  '大時',
+  '天吏'
+] as const
+
+type LevelKey = typeof levelKeyList[number]
+
+const signGodList = [
+  '相日',
+  '月建',
+  '時德',
+  '六合',
+  '天吏',
+  '長生',
+  '官日',
+  '除日',
+  '執日',
+  '開日',
+  '滿日',
+  '民日',
+  '月破',
+  '危日',
+  '建日',
+  '月德',
+  '大會',
+  '守日',
+  '成日',
+  '定日',
+  '德'
+]
+
+function isDe(god: God) {
+  const set = new Set(theDeList)
+  return set.has(god.key)
+}
+
+function getTheSignGodMap() {
+  const map = new Map<string, boolean>()
+  for (const item of levelKeyList) {
+    map.set(item, false)
+  }
+  for (const item of signGodList) {
+    map.set(item, false)
+  }
+  return map
+}
+
+const getGoodBadLevelDict = () => ({
   平日: [
     [0, [11], ['相日', '時德', '六合']],
     [1, [6], ['相日', '六合', '月刑']],
@@ -25,8 +83,8 @@ const getGoodBadLevel = () => ({
     [4, [0], ['天吏', '月刑']]
   ],
   收日: [
-    [0, [2, 8], ['长生', '六合', '劫煞']],
-    [2, [5, 11], ['长生', '劫煞']],
+    [0, [2, 8], ['長生', '六合', '劫煞']],
+    [2, [5, 11], ['長生', '劫煞']],
     [2, [4, 7], ['月害']],
     [3, [0, 6, 9], ['大時']],
     [3, [1, 10], ['月刑']],
@@ -38,13 +96,13 @@ const getGoodBadLevel = () => ({
     [4, [2, 5, 8, 11], ['月煞']]
   ],
   劫煞: [
-    [0, [2, 8], ['长生', '六合', '收日']],
+    [0, [2, 8], ['長生', '六合', '收日']],
     [1, [1, 4, 7, 10], ['除日', '相日']],
-    [2, [5, 11], ['长生', '月害', '收日']],
+    [2, [5, 11], ['長生', '月害', '收日']],
     [3, [0, 3, 6, 9], ['執日']]
   ],
   災煞: [
-    [1, [2, 5, 8, 10], ['开日']],
+    [1, [2, 5, 8, 10], ['開日']],
     [2, [1, 4, 7, 10], ['滿日', '民日']],
     [4, [0, 6], ['月破']],
     [5, [0, 6], ['月厭']]
@@ -68,7 +126,7 @@ const getGoodBadLevel = () => ({
   月害: [
     [2, [3, 9], ['守日', '除日']],
     [2, [1, 7], ['執日', '大時']],
-    [2, [5, 11], ['长生', '劫煞']],
+    [2, [5, 11], ['長生', '劫煞']],
     [2, [8], ['相日', '平日']],
     [3, [0, 6], ['月煞']],
     [3, [4, 10], ['官日', '閉日', '天吏']],
@@ -98,4 +156,29 @@ const getGoodBadLevel = () => ({
   ]
 })
 
-export { theDeList, getGoodBadLevel }
+/**
+ * 取得等次
+ * @param theGods TheGods實例
+ * @param monthBranchValue 月的地支索引值
+ */
+function findLevel(theGods: TheGods, monthBranchValue: number) {
+  const dutyGodKey = theGods.getDuty12God().key + '日'
+  const life12GodKey = theGods.getLife12God('day').key
+  const levelDict = getGoodBadLevelDict()
+  const signGodMap = getTheSignGodMap()
+  if (signGodMap.has(dutyGodKey)) signGodMap.set(dutyGodKey, true)
+  if (dutyGodKey === '建日') signGodMap.set('月建', true)
+  if (signGodMap.has(life12GodKey)) signGodMap.set(dutyGodKey, true)
+
+  // for (the) {
+
+  // }
+  let levelDictItem = null
+  if (levelDict.hasOwnProperty(dutyGodKey)) {
+    // levelDictItem = levelDict[dutyGodKey as LevelKey]
+  } else {
+    //
+  }
+}
+
+export { theDeList, getGoodBadLevelDict }
