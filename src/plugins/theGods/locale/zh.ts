a6350bc8057fec97be1cde4402fc8af029188d9d
Natron Kan <waterbeside@163.com>
2022-10-22 11:59:57 +0800

    i18n: 修改翻译字条

--- src/plugins/theGods/locale/zh.ts
+++ src/plugins/theGods/locale/zh.ts
@@ -13,19 +13,19 @@ export default {
       [GQS.TDG]: '本日神煞',
       [GQS.DBYG]: '本日黃黑十二神',
       [GQS.HBYG]: '此時黃黑十二神',
-      [GQS.DTG]: '十二值神',
+      [GQS.DTG]: '建除十二神',
       [GQS.YLLG]: '年長生十二神',
       [GQS.MLLG]: '月長生十二神',
       [GQS.DLLG]: '日長生十二神',
       [GQS.HLLG]: '時長生十二神',
       [GQS.GA]: '宜',
-      [GQS.GA1]: '宜 1',
-      [GQS.GA2]: '宜 2',
-      [GQS.GA3]: '宜 3',
+      [GQS.GA1]: '宜1',
+      [GQS.GA2]: '宜2',
+      [GQS.GA3]: '宜3',
       [GQS.BA]: '忌',
-      [GQS.BA1]: '忌 1',
-      [GQS.BA2]: '忌 2',
-      [GQS.BA3]: '忌 3'
+      [GQS.BA1]: '忌1',
+      [GQS.BA2]: '忌2',
+      [GQS.BA3]: '忌3'
     }
   }
 }
