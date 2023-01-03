ab489e9e0661a5aa364f394823b3595554b9c7b1
Natron Kan <waterbeside@163.com>
2023-01-03 20:20:16 +0800

    chore: use preset: ts-jest

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from '@jest/types'
 // Sync object
 const config: Config.InitialOptions = {
   verbose: true,
+  preset: 'ts-jest',
   testPathIgnorePatterns: [
     '<rootDir>/node_modules/',
     '<rootDir>/dist/',
