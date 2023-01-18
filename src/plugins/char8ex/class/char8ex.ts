471546b6c8a96acb111c55eff9fc466d36bc67c7
Natron Kan <waterbeside@163.com>
2023-01-18 16:04:48 +0800

    fix: 修正命宫，身命等计算公式

--- src/plugins/char8ex/class/char8ex.ts
+++ src/plugins/char8ex/class/char8ex.ts
@@ -10,14 +10,18 @@ import { trans } from '../utils'
 import { SBX } from '../types'
 import { C8God } from './c8God'
 import { createAllC8Gods } from '../utils/c8Gods'
+import { branchValue2month, month2BranchValue } from '../utils'
 
 export class Char8Ex extends CacheClass {
-  readonly _pillars: [Pillar, Pillar, Pillar, Pillar]
   readonly _lang: string = 'zh'
   readonly me: Stem
   readonly char8: Char8
   readonly lsr: Lunisolar
   readonly sexValue: 0 | 1 | null
+  readonly year: Pillar
+  readonly month: Pillar
+  readonly day: Pillar
+  readonly hour: Pillar
   readonly gods: {
     [x in YMDH]: C8God[]
   } = { year: [], month: [], day: [], hour: [] }
@@ -30,44 +34,35 @@ export class Char8Ex extends CacheClass {
     this.char8 = lsr.char8
     this._lang = lsr.getConfig().lang
     const ymdhList = ['year', 'month', 'day', 'hour']
-    this._pillars = lsr.char8.list.map(
-      (item, index) =>
-        new Pillar({
-          sb: item as unknown as SBX,
-          me: this.me,
-          cate: ymdhList[index] as YMDH,
-          lang: lsr.char8.getConfig().lang
-        })
-    ) as [Pillar, Pillar, Pillar, Pillar]
+    const createPillar = (ymdh: YMDH) => {
+      return new Pillar({
+        sb: this.char8[ymdh] as unknown as SBX,
+        me: this.me,
+        cate: ymdh,
+        lang: lsr.char8.getConfig().lang
+      })
+    }
+    this.year = createPillar('year')
+    this.month = createPillar('month')
+    this.day = createPillar('day')
+    this.hour = createPillar('hour')
     this.gods = createAllC8Gods(this)
     for (let i = 0; i < 4; i++) {
       const ymdh: YMDH = ymdhList[i] as YMDH
-      this._pillars[i]._pushGods(this.gods[ymdh])
+      this[ymdh]._pushGods(this.gods[ymdh])
     }
   }
 
   get list(): [Pillar, Pillar, Pillar, Pillar] {
-    return [...this._pillars]
+    return [this.year, this.month, this.day, this.hour]
   }
 
-  get year(): Pillar {
-    return this._pillars[0]
-  }
-
-  get month(): Pillar {
-    return this._pillars[1]
-  }
-
-  get day(): Pillar {
-    return this._pillars[2]
-  }
-
-  get hour(): Pillar {
-    return this._pillars[3]
-  }
-
-  get sex() {
+  get sex(): string {
     return trans(`sex.${this.sexValue}`, this._lang)
+  }
+
+  get missing() {
+    return this.day.missing
   }
 
   /**
@@ -94,16 +89,19 @@ export class Char8Ex extends CacheClass {
    * 掌上推命宫法，以排山掌子位为正月，
    * 也就是将正月固定在子位上，逆数亥为二月，戌为三月，酉为四月，申为五月，未为六月，午为七月，已为八月，辰为九月，卯为十月，寅为十一月，丑为十二月，
    * 然后再把出生时间安在这个月支上，顺数至卯，卯就是命宫。
+   * 数字计法，寅为1，m = 月+时, m > 14 则26-m, 否则14-m，即为新的月份数，其所在地支为身宫地
    */
   @cache('char8ex:ownSign')
   ownSign(): SB {
     // const month = this.lsr.lunar.month
-    const month = ((this.month.branch.value + 10) % 12) + 1
-    const c8hour = this.hour.branch.value
-    const monthSign = (13 - month) % 12
-    const b = (12 + 3 - c8hour + monthSign) % 12
-    // 命宫天干按五虎遁（五鼠遁+2）
-    const s = (computeRatStem(this.year.stem.value, this.month.branch.value) + 2) % 10
+    const month = branchValue2month(this.month.branch.value)
+    const c8hour = branchValue2month(this.hour.branch.value)
+    // const monthSign = (13 - month) % 12
+    // const b = (12 + 3 - c8hour + monthSign) % 12
+    const newMonth = month + c8hour
+    const b = month2BranchValue(newMonth > 14 ? 26 - newMonth : 14 - newMonth)
+    // 命宫天干按五虎遁（五虎遁和五鼠遁本质是一样的）
+    const s = computeRatStem(this.year.stem.value, b) % 10
     return new SB(s, b, { lang: this.char8.getConfig().lang })
   }
 
@@ -114,21 +112,23 @@ export class Char8Ex extends CacheClass {
    身宫口诀记心中，子起正月顺查行。
    生月支上起生时，逆推至酉知身宫。
    生月天干两头查，顺逆宫支天干停。
+
+   子上起正月，顺推到本命生月，生时落生月，逆推至酉
    ```
+   * 数学计算法: m = (月份数 + 时辰序号) % 12
    */
   @cache('char8ex:bodySign')
   bodySign(): SB {
     // const month = this.lsr.lunar.month
-    const month = ((this.month.branch.value + 10) % 12) + 1
-    const c8hour = this.hour.branch.value
-    const monthSign = (month - 1) % 12
-    const b = (12 + c8hour - 9 + monthSign) % 12
+    const month = branchValue2month(this.month.branch.value)
+    const c8hour = this.hour.branch.value + 1
+    const b = month2BranchValue((month + c8hour) % 12)
     // 查身宫天干
-    const s = (10 + this.month.stem.value + b - this.month.branch.value) % 10
+    const s = computeRatStem(this.year.stem.value, b) % 10
     return new SB(s, b, { lang: this.char8.getConfig().lang })
   }
 
   toString() {
-    return `${this.sex}:\n${this.year} ${this.month} ${this.day} ${this.hour}`
+    return `${this.sex}: ${this.year} ${this.month} ${this.day} ${this.hour}`
   }
 }
