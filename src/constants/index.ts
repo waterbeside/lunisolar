01824b695b3ac62d3d9d89eec959311debf30a0e
Natron Kan <waterbeside@163.com>
2023-09-04 15:19:48 +0800

    feat: format方法支持通过dRr取得该星期几是倒数的第几个星期几

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -36,4 +36,4 @@ export const REGEX_PARSE =
   /^(\d{4})[-/]?(\d{1,2})?[-/]?(\d{0,2})[Tt\s]*(\d{1,2})?:?(\d{1,2})?:?(\d{1,2})?[.:]?(\d+)?$/
 // format
 export const REGEX_FORMAT =
-  /\[([^\]]+)]|cZ|t|T|lYn|lMn|lDn|lHn|lY|lM|lL|lD|lH|cYs|cYb|cY|cMs|cMb|cM|cDs|cDb|cD|cHs|cHb|cH|dR|Y{1,4}|M{1,4}|D{1,2}|d{1,4}|H{1,2}|h{1,2}|a|A|m{1,2}|s{1,2}|Z{1,2}|SSS/g
+  /\[([^\]]+)]|cZ|t|T|lYn|lMn|lDn|lHn|lY|lM|lL|lD|lH|cYs|cYb|cY|cMs|cMb|cM|cDs|cDb|cD|cHs|cHb|cH|dRr|dR|Y{1,4}|M{1,4}|D{1,2}|d{1,4}|H{1,2}|h{1,2}|a|A|m{1,2}|s{1,2}|Z{1,2}|SSS/g
