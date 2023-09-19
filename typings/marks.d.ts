c3928b892248fcee5c78ce8e21fecbb1a1b40df3
Natron Kan <waterbeside@163.com>
2023-09-19 10:39:35 +0800

    types: 更改markers类型设定

--- typings/marks.d.ts
+++ typings/marks.d.ts
@@ -1,12 +1,18 @@
-type MarkerMap = {
-  format: string
-  map: Map<string, MarkerMapDateDictItem[]>
+type MarkersSettingItem = {
+  tag: string | string[]
+  data?: {
+    [key: string]: any
+  }
+  name: string
 }
 
-type MarkerMapDateDictItem = {
-  tag: Set<string>
-  data: any
-  desc: string
+type MarkerFormatFun = (lsr: lunisolar.Lunisolar) => unknown
+
+type MarkersSetting = {
+  format: string | MarkerFormatFun
+  markers: {
+    [key: string]: MarkersSettingItem | MarkersSettingItem[]
+  }
 }
 
-type MarkerSetting = {}
+type MarkerSetting = Pick<MarkersSettingItem, 'name'> & Partial<Omit<MarkersSettingItem, 'name'>>
