8db3bd0c0fa366c61f2a8d5191785bb4f075b2db
Natron Kan <waterbeside@163.com>
2023-11-13 15:25:53 +0800

    wip(markers): 更改节日tag

--- src/markers/festivals.zh-cn.ts
+++ src/markers/festivals.zh-cn.ts
@@ -4,405 +4,405 @@ const festivals: MarkersSetting = [
     format: 'MMDD',
     markers: {
       '0101': {
-        tag: '传统节日',
+        tag: ['cn', 'traditional'],
         name: '元旦'
       },
       '0110': {
-        tag: ['中国主题', 'occupation'],
+        tag: ['cn', 'occupation'],
         name: '中国人民警察节'
       },
       '0121': {
-        tag: '国际主题',
+        tag: 'international',
         name: '国际拥抱日'
       },
       '0202': {
-        tag: ['国际主题', 'environment'],
+        tag: ['international', 'environment'],
         name: '世界湿地日'
       },
       '0204': {
-        tag: ['国际主题', 'health'],
+        tag: ['international', 'health'],
         name: '世界抗癌日'
       },
       '0210': {
-        tag: '国际主题',
+        tag: 'international',
         name: '国际气象节'
       },
       '0214': {
-        tag: '西方节日',
+        tag: 'western',
         name: '情人节'
       },
       '0220': {
-        tag: '国际主题',
+        tag: 'international',
         name: '世界社会公正日'
       },
       '0301': {
-        tag: ['国际主题', 'environment'],
+        tag: ['international', 'environment'],
         name: '国际海豹日'
       },
       '0303': [
         {
-          tag: ['国际主题', 'environment'],
+          tag: ['international', 'environment'],
           name: '世界野生动植物日'
         },
         {
-          tag: ['中国主题', 'health'],
+          tag: ['cn', 'health'],
           name: '全国爱耳日'
         },
         {
-          tag: ['国际主题', 'health'],
+          tag: ['international', 'health'],
           name: '国际爱耳日'
         }
       ],
       '0305': {
-        tag: ['中国主题', 'commemoration'],
+        tag: ['cn', 'commemoration'],
         name: '学雷锋记念日'
       },
       '0306': {
-        tag: ['国际主题', 'health'],
+        tag: ['international', 'health'],
         name: '世界青光眼日'
       },
       '0308': {
-        tag: '国际主题',
+        tag: 'international',
         name: '妇女节'
       },
       '0312': {
-        tag: ['中国主题', 'environment'],
+        tag: ['cn', 'environment'],
         name: '植树节'
       },
       '0315': {
-        tag: '国际主题',
+        tag: 'international',
         name: '国际消费日'
       },
       '0320': [
         {
-          tag: '国际主题',
+          tag: 'international',
           name: '世界无肉日'
         },
         {
-          tag: '国际主题',
+          tag: 'international',
           name: '国际幸福日'
         }
       ],
       '0321': [
         {
-          tag: ['国际主题', 'environment'],
+          tag: ['international', 'environment'],
           name: '世界森林日'
         },
         {
-          tag: '国际主题',
+          tag: 'international',
           name: '世界睡眠日'
         },
         {
-          tag: '国际主题',
+          tag: 'international',
           name: '世界儿歌日'
         },
         {
-          tag: '国际主题',
+          tag: 'international',
           name: '国际消除种族歧视日'
         }
       ],
       '0322': {
-        tag: ['国际主题', 'environment'],
+        tag: ['international', 'environment'],
         name: '世界水日'
       },
       '0323': {
-        tag: '国际主题',
+        tag: 'international',
         name: '世界气象日'
       },
       '0324': {
-        tag: ['国际主题', 'health'],
+        tag: ['international', 'health'],
         name: '世界防治结核病日'
       },
       '0327': {
-        tag: '国际主题',
+        tag: 'international',
         name: '世界戏剧日'
       },
       '0401': [
         {
-          tag: ['国际主题', 'health'],
+          tag: ['international', 'health'],
           name: '国际爱鸟日'
         },
         {
-          tag: '西方节日',
+          tag: 'western',
           name: '愚人节'
         }
       ],
       '0407': {
-        tag: '国际主题',
+        tag: 'international',
         name: '世界卫生日'
       },
       '0408': {
-        tag: ['国际主题', 'environment'],
+        tag: ['international', 'environment'],
         name: '国际珍稀动物保护日'
       },
       '0415': {
-        tag: '中国主题',
+        tag: 'cn',
         name: '国家安全教育日'
       },
       '0422': {
-        tag: ['国际主题', 'environment'],
+        tag: ['international', 'environment'],
         name: '世界地球日'
       },
       '0423': {
-        tag: '国际主题',
+        tag: 'international',
         name: '世界读书日'
       },
       '0501': {
-        tag: '国际主题',
+        tag: 'international',
         name: '国际劳动节'
       },
       '0504': {
-        tag: '中国主题',
+        tag: 'cn',
         name: '中国青年节'
       },
       '0508': {
-        tag: ['国际主题', 'health'],
+        tag: ['international', 'health'],
         name: '世界红十字日'
       },
       '0511': {
-        tag: ['国际主题', 'health'],
+        tag: ['international', 'health'],
         name: '防治肥胖日'
       },
       '0512': [
         {
-          tag: ['国际主题', 'occupation'],
+          tag: ['international', 'occupation'],
           name: '国际护士节'
         },
         {
-          tag: '中国主题',
+          tag: 'cn',
           name: '全国防灾减灾日'
         }
       ],
       '0515': {
-        tag: ['国际主题', 'health'],
+        tag: ['international', 'health'],
         name: '全国碘缺乏病日'
       },
       '0517': {
-        tag: '国际主题',
+        tag: 'international',
         name: '世界电信日'
       },
       '0518': {
-        tag: '国际主题',
+        tag: 'international',
         name: '国际博物馆日'
       },
       '0520': [
         {
-          tag: '中国主题',
+          tag: 'cn',
           name: '全国母乳喂养宣传日'
         },
         {
-          tag: '中国主题',
+          tag: 'cn',
           name: '中国学生营养日'
         }
       ],
       '0525': {
-        tag: ['国际主题', 'health'],
+        tag: ['international', 'health'],
         name: '国际爱肤日'
       },
       '0531': {
-        tag: ['国际主题', 'health'],
+        tag: ['international', 'health'],
         name: '世界无烟日'
       },
       '0601': {
-        tag: '中国节日',
+        tag: 'cn',
         name: '儿童节'
       },
       '0605': {
-        tag: ['国际主题', 'environment'],
+        tag: ['international', 'environment'],
         name: '世界环境日'
       },
       '0606': {
-        tag: ['中国主题', 'health'],
+        tag: ['cn', 'health'],
         name: '全国爱眼日'
       },
       '0609': {
-        tag: '国际主题',
+        tag: 'international',
         name: '世界认可日'
       },
       '0623': {
-        tag: ['国际主题', 'sport'],
+        tag: ['international', 'sport'],
         name: '国际奥林匹克日'
       },
       '0625': {
-        tag: '中国主题',
+        tag: 'cn',
         name: '全国土地日'
       },
       '0626': {
-        tag: '国际主题',
+        tag: 'international',
         name: '国际反毒品日'
       },
       '0701': [
         {
-          tag: ['中国节日', 'commemoration'],
+          tag: ['cn', 'commemoration'],
           name: '香港回归日'
         },
         {
-          tag: ['中国节日', 'commemoration'],
+          tag: ['cn', 'commemoration'],
           name: '建党节'
         }
       ],
       '0711': [
         {
-          tag: '国际主题',
+          tag: 'international',
           name: '世界人口日'
         },
         {
-          tag: '中国主题',
+          tag: 'cn',
           name: '中国航海日'
         }
       ],
       '0801': {
-        tag: ['中国节日', 'commemoration'],
+        tag: ['cn', 'commemoration'],
         name: '建军节'
       },
       '0812': {
-        tag: '国际主题',
+        tag: 'international',
         name: '国际青年节'
       },
       '0819': {
-        tag: ['中国节日', 'occupation'],
+        tag: ['cn', 'occupation'],
         name: '中国医师节'
       },
       '0903': {
-        tag: ['中国节日', 'commemoration'],
+        tag: ['cn', 'commemoration'],
         name: '中国人民抗日战争胜利纪念日'
       },
       '0910': {
-        tag: ['中国节日', 'occupation'],
+        tag: ['cn', 'occupation'],
         name: '教师节'
       },
       '0912': {
-        tag: ['中国主题', 'health'],
+        tag: ['cn', 'health'],
         name: '预防出生缺陷日'
       },
       '0920': {
-        tag: ['中国主题', 'health'],
+        tag: ['cn', 'health'],
         name: '全国爱牙日'
       },
       '0921': {
-        tag: '国际主题',
+        tag: 'international',
         name: '国际和平日'
       },
       '0927': {
-        tag: '国际主题',
+        tag: 'international',
         name: '世界旅游日'
       },
       '0930': {
-        tag: ['中国节日', 'commemoration'],
+        tag: ['cn', 'commemoration'],
         name: '列士纪念日'
       },
       '1001': [
         {
-          tag: '国际主题',
+          tag: 'international',
           name: '国际音乐节'
         },
         {
-          tag: '中国节日',
+          tag: 'cn',
           name: '国庆节'
         }
       ],
       '1004': {
-        tag: ['国际主题', 'environment'],
+        tag: ['international', 'environment'],
         name: '世界动物日'
       },
       '1007': {
-        tag: '国际主题',
+        tag: 'international',
         name: '国际住房日'
       },
       '1009': {
-        tag: '国际主题',
+        tag: 'international',
         name: '世界邮政日'
       },
       '1010': {
-        tag: ['国际主题', 'health'],
+        tag: ['international', 'health'],
         name: '世界精神卫生日'
       },
       '1013': {
-        tag: ['国际主题', 'health'],
+        tag: ['international', 'health'],
         name: '世界保健日'
       },
       '1015': {
-        tag: '国际主题',
+        tag: 'international',
         name: '国际盲人节'
       },
       '1016': {
-        tag: '国际主题',
+        tag: 'international',
         name: '世界粮食日'
       },
       '1017': {
-        tag: '国际主题',
+        tag: 'international',
         name: '世界消除贫困日'
       },
       '1024': [
         {
-          tag: '国际主题',
+          tag: 'international',
           name: '联合国日'
         },
         {
-          tag: ['国际主题', 'occupation'],
+          tag: ['international', 'occupation'],
           name: '程序员节'
         }
       ],
       '1101': {
-        tag: '西方节日',
+        tag: 'western',
         name: '万圣节'
       },
       '1108': {
-        tag: ['中国主题', 'occupation'],
+        tag: ['cn', 'occupation'],
         name: '中国记者日'
       },
       '1109': {
-        tag: ['中国主题'],
+        tag: ['cn'],
         name: '消防宣传日'
       },
       '1114': {
-        tag: ['国际主题', 'health'],
+        tag: ['international', 'health'],
         name: '世界糖尿病日'
       },
       '1117': {
-        tag: ['国际主题', 'occupation'],
+        tag: ['international', 'occupation'],
         name: '国际大学生节'
       },
       '1201': {
-        tag: ['国际主题', 'health'],
+        tag: ['international', 'health'],
         name: '世界爱滋病日'
       },
       '1203': {
-        tag: ['国际主题', 'health'],
+        tag: ['international', 'health'],
         name: '世界残疾人日'
       },
       '1204': [
         {
-          tag: ['中国主题'],
+          tag: ['cn'],
           name: '国家宪法日'
         },
         {
-          tag: ['中国主题'],
+          tag: ['cn'],
           name: '全国法制宣传日'
         }
       ],
       '1209': {
-        tag: ['国际主题', 'sport'],
+        tag: ['international', 'sport'],
         name: '世界足球日'
       },
       '1213': {
-        tag: ['中国主题', 'commemoration'],
+        tag: ['cn', 'commemoration'],
         name: '南京大屠杀死难者国家公镇祭日'
       },
       '1220': {
-        tag: ['中国主题', 'commemoration'],
+        tag: ['cn', 'commemoration'],
         name: '澳门回归纪念日'
       },
       '1221': {
-        tag: ['国际主题', 'sport'],
+        tag: ['international', 'sport'],
         name: '国际篮球日'
       },
       '1224': {
-        tag: ['西方节日'],
+        tag: ['western'],
         name: '平安夜'
       },
       '1225': {
-        tag: ['西方节日'],
+        tag: ['western'],
         name: '圣诞节'
       }
     }
@@ -411,39 +411,39 @@ const festivals: MarkersSetting = [
     format: 'M,d,dR',
     markers: {
       '3,4,2': {
-        tag: ['国际主题', 'health'],
+        tag: ['international', 'health'],
         name: '世界肾脏日'
       },
       '3,2,3': {
-        tag: '国际主题',
+        tag: 'international',
         name: '世界社会工作日'
       },
       '5,2,1': {
-        tag: ['国际主题', 'health'],
+        tag: ['international', 'health'],
         name: '世界防治哮喘日'
       },
       '5,0,2': {
-        tag: '西方节日',
+        tag: 'western',
         name: '母亲节'
       },
       '5,0,3': {
-        tag: ['中国主题', 'health'],
+        tag: ['cn', 'health'],
         name: '全国助残日'
       },
       '6,0,3': {
-        tag: '西方节日',
+        tag: 'western',
         name: '父亲节'
       },
       '9,0,4': {
-        tag: ['国际主题', 'health'],
+        tag: ['international', 'health'],
         name: '国际聋人节'
       },
       '10,4,2': {
-        tag: ['国际主题', 'health'],
+        tag: ['international', 'health'],
         name: '世界视力日'
       },
       '11,4,4': {
-        tag: '西方节日',
+        tag: 'western',
         name: '感恩节'
       }
     }
@@ -452,11 +452,11 @@ const festivals: MarkersSetting = [
     format: 'M,d,dRr',
     markers: {
       '1,0,1': {
-        tag: ['国际主题', 'health'],
+        tag: ['international', 'health'],
         name: '国际麻风节'
       },
       '3,1,1': {
-        tag: '中国主题',
+        tag: 'cn',
         name: '中小学安全教育日'
       }
     }
@@ -465,35 +465,35 @@ const festivals: MarkersSetting = [
     format: 'lMn,lDn',
     markers: {
       '1,1': {
-        tag: '传统节日',
+        tag: ['cn', 'traditional'],
         name: '春节'
       },
       '2,2': {
-        tag: '传统节日',
+        tag: ['cn', 'traditional'],
         name: '龙抬头'
       },
       '5,5': {
-        tag: '传统节日',
+        tag: ['cn', 'traditional'],
         name: '端午节'
       },
       '7,7': {
-        tag: '传统节日',
+        tag: ['cn', 'traditional'],
         name: '七夕节'
       },
       '7,14': {
-        tag: '传统节日',
+        tag: ['cn', 'traditional'],
         name: '中元节'
       },
       '8,15': {
-        tag: '传统节日',
+        tag: ['cn', 'traditional'],
         name: '中秋节'
       },
       '9,9': {
-        tag: '传统节日',
+        tag: ['cn', 'traditional'],
         name: '重阳节'
       },
       '12,8': {
-        tag: '传统节日',
+        tag: ['cn', 'traditional'],
         name: '腊八节'
       }
     }
@@ -505,7 +505,7 @@ const festivals: MarkersSetting = [
     },
     markers: {
       t: {
-        tag: ['国际主题', 'health'],
+        tag: ['international', 'health'],
         name: '国际罕见病日'
       }
     }
--- src/markers/festivals.zh.ts
+++ src/markers/festivals.zh.ts
@@ -4,405 +4,405 @@ const festivals: MarkersSetting = [
     format: 'MMDD',
     markers: {
       '0101': {
-        tag: '傳統節日',
+        tag: ['cn', 'traditional'],
         name: '元旦'
       },
       '0110': {
-        tag: ['中國主題', 'occupation'],
+        tag: ['cn', 'occupation'],
         name: '中国人民警察节'
       },
       '0121': {
-        tag: '國際主題',
+        tag: 'international',
         name: '國際擁抱日'
       },
       '0202': {
-        tag: ['國際主題', 'environment'],
+        tag: ['international', 'environment'],
         name: '世界濕地日'
       },
       '0204': {
-        tag: ['國際主題', 'health'],
+        tag: ['international', 'health'],
         name: '世界抗癌日'
       },
       '0210': {
-        tag: '國際主題',
+        tag: 'international',
         name: '國際氣象節'
       },
       '0214': {
-        tag: '西方節日',
+        tag: 'western',
         name: '情人節'
       },
       '0220': {
-        tag: '國際主題',
+        tag: 'international',
         name: '世界社會公正日'
       },
       '0301': {
-        tag: ['國際主題', 'environment'],
+        tag: ['international', 'environment'],
         name: '國際海豹日'
       },
       '0303': [
         {
-          tag: ['國際主題', 'environment'],
+          tag: ['international', 'environment'],
           name: '世界野生動植物日'
         },
         {
-          tag: ['中國主題', 'health'],
+          tag: ['cn', 'health'],
           name: '全國愛耳日'
         },
         {
-          tag: ['國際主題', 'health'],
+          tag: ['international', 'health'],
           name: '國際愛耳日'
         }
       ],
       '0305': {
-        tag: ['中國主題', 'commemoration'],
+        tag: ['cn', 'commemoration'],
         name: '學雷鋒記念日'
       },
       '0306': {
-        tag: ['國際主題', 'health'],
+        tag: ['international', 'health'],
         name: '世界青光眼日'
       },
       '0308': {
-        tag: '國際主題',
+        tag: 'international',
         name: '婦女節'
       },
       '0312': {
-        tag: ['中國主題', 'environment'],
+        tag: ['cn', 'environment'],
         name: '植樹節'
       },
       '0315': {
-        tag: '國際主題',
+        tag: 'international',
         name: '國際消費日'
       },
       '0320': [
         {
-          tag: '國際主題',
+          tag: 'international',
           name: '世界無肉日'
         },
         {
-          tag: '國際主題',
+          tag: 'international',
           name: '國際幸福日'
         }
       ],
       '0321': [
         {
-          tag: ['國際主題', 'environment'],
+          tag: ['international', 'environment'],
           name: '世界森林日'
         },
         {
-          tag: '國際主題',
+          tag: 'international',
           name: '世界睡眠日'
         },
         {
-          tag: '國際主題',
+          tag: 'international',
           name: '世界兒歌日'
         },
         {
-          tag: '國際主題',
+          tag: 'international',
           name: '國際消除種族歧視日'
         }
       ],
       '0322': {
-        tag: ['國際主題', 'environment'],
+        tag: ['international', 'environment'],
         name: '世界水日'
       },
       '0323': {
-        tag: '國際主題',
+        tag: 'international',
         name: '世界氣象日'
       },
       '0324': {
-        tag: ['國際主題', 'health'],
+        tag: ['international', 'health'],
         name: '世界防治結核病日'
       },
       '0327': {
-        tag: '國際主題',
+        tag: 'international',
         name: '世界戲劇日'
       },
       '0401': [
         {
-          tag: ['國際主題', 'health'],
+          tag: ['international', 'health'],
           name: '國際愛鳥日'
         },
         {
-          tag: '西方節日',
+          tag: 'western',
           name: '愚人節'
         }
       ],
       '0407': {
-        tag: '國際主題',
+        tag: 'international',
         name: '世界衛生日'
       },
       '0408': {
-        tag: ['國際主題', 'environment'],
+        tag: ['international', 'environment'],
         name: '國際珍稀動物保護日'
       },
       '0415': {
-        tag: '中國主題',
+        tag: 'cn',
         name: '國家安全教育日'
       },
       '0422': {
-        tag: ['國際主題', 'environment'],
+        tag: ['international', 'environment'],
         name: '世界地球日'
       },
       '0423': {
-        tag: '國際主題',
+        tag: 'international',
         name: '世界讀書日'
       },
       '0501': {
-        tag: '國際主題',
+        tag: 'international',
         name: '國際勞動節'
       },
       '0504': {
-        tag: '中國主題',
+        tag: 'cn',
         name: '中國青年節'
       },
       '0508': {
-        tag: ['國際主題', 'health'],
+        tag: ['international', 'health'],
         name: '世界紅十字日'
       },
       '0511': {
-        tag: ['國際主題', 'health'],
+        tag: ['international', 'health'],
         name: '防治肥胖日'
       },
       '0512': [
         {
-          tag: ['國際主題', 'occupation'],
+          tag: ['international', 'occupation'],
           name: '國際護士節'
         },
         {
-          tag: '中國主題',
+          tag: 'cn',
           name: '全國防災減災日'
         }
       ],
       '0515': {
-        tag: ['國際主題', 'health'],
+        tag: ['cn', 'health'],
         name: '全國碘缺乏病日'
       },
       '0517': {
-        tag: '國際主題',
+        tag: 'international',
         name: '世界電信日'
       },
       '0518': {
-        tag: '國際主題',
+        tag: 'international',
         name: '國際博物館日'
       },
       '0520': [
         {
-          tag: '中國主題',
+          tag: 'cn',
           name: '全國母乳餵養宣傳日'
         },
         {
-          tag: '中國主題',
+          tag: 'cn',
           name: '中國學生營養日'
         }
       ],
       '0525': {
-        tag: ['國際主題', 'health'],
+        tag: ['international', 'health'],
         name: '國際愛膚日'
       },
       '0531': {
-        tag: ['國際主題', 'health'],
+        tag: ['international', 'health'],
         name: '世界無煙日'
       },
       '0601': {
-        tag: '中國節日',
+        tag: 'cn',
         name: '兒童節'
       },
       '0605': {
-        tag: ['國際主題', 'environment'],
+        tag: ['international', 'environment'],
         name: '世界環境日'
       },
       '0606': {
-        tag: ['中國主題', 'health'],
+        tag: ['cn', 'health'],
         name: '全國愛眼日'
       },
       '0609': {
-        tag: '國際主題',
+        tag: 'international',
         name: '世界認可日'
       },
       '0623': {
-        tag: ['國際主題', 'sport'],
+        tag: ['international', 'sport'],
         name: '國際奧林匹克日'
       },
       '0625': {
-        tag: '中國主題',
+        tag: 'cn',
         name: '全國土地日'
       },
       '0626': {
-        tag: '國際主題',
+        tag: 'international',
         name: '國際反毒品日'
       },
       '0701': [
         {
-          tag: ['中國節日', 'commemoration'],
+          tag: ['cn', 'commemoration'],
           name: '香港回歸日'
         },
         {
-          tag: ['中國節日', 'commemoration'],
+          tag: ['cn', 'commemoration'],
           name: '建黨節'
         }
       ],
       '0711': [
         {
-          tag: '國際主題',
+          tag: 'international',
           name: '世界人口日'
         },
         {
-          tag: '中國主題',
+          tag: 'cn',
           name: '中國航海日'
         }
       ],
       '0801': {
-        tag: ['中國節日', 'commemoration'],
+        tag: ['cn', 'commemoration'],
         name: '建軍節'
       },
       '0812': {
-        tag: '國際主題',
+        tag: 'international',
         name: '國際青年節'
       },
       '0819': {
-        tag: ['中國節日', 'occupation'],
+        tag: ['cn', 'occupation'],
         name: '中國醫師節'
       },
       '0903': {
-        tag: ['中國節日', 'commemoration'],
+        tag: ['cn', 'commemoration'],
         name: '中國人民抗日戰爭勝利紀念日'
       },
       '0910': {
-        tag: ['中國節日', 'occupation'],
+        tag: ['cn', 'occupation'],
         name: '教師節'
       },
       '0912': {
-        tag: ['中國主題', 'health'],
+        tag: ['cn', 'health'],
         name: '預防出生缺陷日'
       },
       '0920': {
-        tag: ['中國主題', 'health'],
+        tag: ['cn', 'health'],
         name: '全國愛牙日'
       },
       '0921': {
-        tag: '國際主題',
+        tag: 'international',
         name: '國際和平日'
       },
       '0927': {
-        tag: '國際主題',
+        tag: 'international',
         name: '世界旅遊日'
       },
       '0930': {
-        tag: ['中國節日', 'commemoration'],
+        tag: ['cn', 'commemoration'],
         name: '列士纪念日'
       },
       '1001': [
         {
-          tag: '國際主題',
+          tag: 'international',
           name: '國際音樂節'
         },
         {
-          tag: '中國節日',
+          tag: 'cn',
           name: '國慶節'
         }
       ],
       '1004': {
-        tag: ['國際主題', 'environment'],
+        tag: ['international', 'environment'],
         name: '世界動物日'
       },
       '1007': {
-        tag: '國際主題',
+        tag: 'international',
         name: '國際住房日'
       },
       '1009': {
-        tag: '國際主題',
+        tag: 'international',
         name: '世界郵政日'
       },
       '1010': {
-        tag: ['國際主題', 'health'],
+        tag: ['international', 'health'],
         name: '世界精神衛生日'
       },
       '1013': {
-        tag: ['國際主題', 'health'],
+        tag: ['international', 'health'],
         name: '世界保健日'
       },
       '1015': {
-        tag: '國際主題',
+        tag: 'international',
         name: '國際盲人節'
       },
       '1016': {
-        tag: '國際主題',
+        tag: 'international',
         name: '世界糧食日'
       },
       '1017': {
-        tag: '國際主題',
+        tag: 'international',
         name: '世界消除貧困日'
       },
       '1024': [
         {
-          tag: '國際主題',
+          tag: 'international',
           name: '聯合國日'
         },
         {
-          tag: ['國際主題', 'occupation'],
+          tag: ['international', 'occupation'],
           name: '程序員节'
         }
       ],
       '1101': {
-        tag: '西方節日',
+        tag: 'western',
         name: '萬聖節'
       },
       '1108': {
-        tag: ['中國主題', 'occupation'],
+        tag: ['cn', 'occupation'],
         name: '中國記者日'
       },
       '1109': {
-        tag: ['中國主題'],
+        tag: ['cn'],
         name: '消防宣傳日'
       },
       '1114': {
-        tag: ['國際主題', 'health'],
+        tag: ['international', 'health'],
         name: '世界糖尿病日'
       },
       '1117': {
-        tag: ['國際主題', 'occupation'],
+        tag: ['international', 'occupation'],
         name: '國際大學生節'
       },
       '1201': {
-        tag: ['國際主題', 'health'],
+        tag: ['international', 'health'],
         name: '世界愛滋病日'
       },
       '1203': {
-        tag: ['國際主題', 'health'],
+        tag: ['international', 'health'],
         name: '世界殘疾人日'
       },
       '1204': [
         {
-          tag: ['中國主題'],
+          tag: ['cn'],
           name: '國家憲法日'
         },
         {
-          tag: ['中國主題'],
+          tag: ['cn'],
           name: '全國法制宣傳日'
         }
       ],
       '1209': {
-        tag: ['國際主題', 'sport'],
+        tag: ['international', 'sport'],
         name: '世界足球日'
       },
       '1213': {
-        tag: ['中國主題', 'commemoration'],
+        tag: ['cn', 'commemoration'],
         name: '南京大屠殺死難者國家公鎮祭日'
       },
       '1220': {
-        tag: ['中國主題', 'commemoration'],
+        tag: ['cn', 'commemoration'],
         name: '澳門回歸紀念日'
       },
       '1221': {
-        tag: ['國際主題', 'sport'],
+        tag: ['international', 'sport'],
         name: '國際籃球日'
       },
       '1224': {
-        tag: ['西方節日'],
+        tag: ['western'],
         name: '平安夜'
       },
       '1225': {
-        tag: ['西方節日'],
+        tag: ['western'],
         name: '聖誕節'
       }
     }
@@ -411,39 +411,39 @@ const festivals: MarkersSetting = [
     format: 'M,d,dR',
     markers: {
       '3,4,2': {
-        tag: ['國際主題', 'health'],
+        tag: ['international', 'health'],
         name: '世界腎臟日'
       },
       '3,2,3': {
-        tag: '國際主題',
+        tag: 'international',
         name: '世界社會工作日'
       },
       '5,2,1': {
-        tag: ['國際主題', 'health'],
+        tag: ['international', 'health'],
         name: '世界防治哮喘日'
       },
       '5,0,2': {
-        tag: '西方節日',
+        tag: 'western',
         name: '母親節'
       },
       '5,0,3': {
-        tag: ['中國主題', 'health'],
+        tag: ['cn', 'health'],
         name: '全國助殘日'
       },
       '6,0,3': {
-        tag: '西方節日',
+        tag: 'western',
         name: '父親節'
       },
       '9,0,4': {
-        tag: ['國際主題', 'health'],
+        tag: ['international', 'health'],
         name: '國際聾人節'
       },
       '10,4,2': {
-        tag: ['國際主題', 'health'],
+        tag: ['international', 'health'],
         name: '世界視力日'
       },
       '11,4,4': {
-        tag: '西方節日',
+        tag: 'western',
         name: '感恩節'
       }
     }
@@ -452,11 +452,11 @@ const festivals: MarkersSetting = [
     format: 'M,d,dRr',
     markers: {
       '1,0,1': {
-        tag: ['國際主題', 'health'],
+        tag: ['international', 'health'],
         name: '國際麻風節'
       },
       '3,1,1': {
-        tag: '中國主題',
+        tag: 'cn',
         name: '中小學安全教育日'
       }
     }
@@ -465,35 +465,35 @@ const festivals: MarkersSetting = [
     format: 'lMn,lDn',
     markers: {
       '1,1': {
-        tag: '傳統節日',
+        tag: ['cn', 'traditional'],
         name: '春節'
       },
       '2,2': {
-        tag: '傳統節日',
+        tag: ['cn', 'traditional'],
         name: '龍抬頭'
       },
       '5,5': {
-        tag: '傳統節日',
+        tag: ['cn', 'traditional'],
         name: '端午節'
       },
       '7,7': {
-        tag: '傳統節日',
+        tag: ['cn', 'traditional'],
         name: '七夕節'
       },
       '7,14': {
-        tag: '傳統節日',
+        tag: ['cn', 'traditional'],
         name: '中元節'
       },
       '8,15': {
-        tag: '傳統節日',
+        tag: ['cn', 'traditional'],
         name: '中秋節'
       },
       '9,9': {
-        tag: '傳統節日',
+        tag: ['cn', 'traditional'],
         name: '重陽節'
       },
       '12,8': {
-        tag: '傳統節日',
+        tag: ['cn', 'traditional'],
         name: '腊八节'
       }
     }
@@ -505,7 +505,7 @@ const festivals: MarkersSetting = [
     },
     markers: {
       t: {
-        tag: ['國際主題', 'health'],
+        tag: ['international', 'health'],
         name: '國際罕見病日'
       }
     }
